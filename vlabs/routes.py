adc0978930e5b0e1717cb06b109b900895c9d16c
Blaze <16274366+Blazephoenix@users.noreply.github.com>
2020-03-02 14:02:13 +0530

    changes to UI

--- vlabs/routes.py
+++ vlabs/routes.py
@@ -135,3 +135,7 @@ def page_not_found(e):
 @app.errorhandler(403)
 def no_permission(e):
     return render_template('pages/errors/403.html')
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('pages/errors/500.html')
